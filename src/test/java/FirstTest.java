8db9ac224eb8eff8499672eda2749e5590870a90
Pavel Rybakov <pav@finch-melrose.com>
2019-07-26 18:44:09 +0300

    Commit for ex2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -13,11 +18,11 @@ public class FirstTest {
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("platformName", "Android");
         cap.setCapability("deviceName", "AndroidTestDevice");
-        //cap.setCapability("uid","HT6B70200690");
+        //cap.setCapability("uid","192.168.56.103:5555");
         cap.setCapability("platformVersion", "8.0");
         cap.setCapability("automationName", "Appium");
         cap.setCapability("appPackage", "org.wikipedia");
@@ -28,12 +33,115 @@ public class FirstTest {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         if (driver != null) driver.quit();
     }
 
     @Test
-    public void firstTest(){
-        System.out.println("First Test run");
+    public void searchTest(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find wikipedia search",
+                5
+        );
+        waitForElement(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Cannot send wikipedia search",
+                5
+        );
+//        waitForElement(
+//            By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+//                "Cannot Find about Java",
+//                15
+//        );
+    }
+    @Test
+    public void testCancelSearch(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot found search input",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_container"),
+                "Java",
+                "Cannot send keys",
+                5
+        );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot clear field",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X on page",
+                5
+        );
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X still presented on this page",
+                5
+        );
+    }
+
+    @Test
+    public void testCompareArticleTitle(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find wikipedia search",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot send wikipedia search",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot Find about Java",
+                10
+        );
+        WebElement title_element = waitForElement(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find title",
+                15
+
+        );
+
+        String acticle_title = title_element.getAttribute("text");
+
+        Assert.assertEquals("Cannot get title", "", "article_title");
+    }
+    private WebElement waitForElement(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElement(By by, String error_message) {
+        return waitForElement(by, error_message);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElement(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElement(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+    private WebElement waitForElementAndClear( By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElement(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
     }
 }

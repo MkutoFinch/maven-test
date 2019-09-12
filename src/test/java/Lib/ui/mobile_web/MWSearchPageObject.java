460f856b2097ccd792bc45a2335a4cad5a96df23
Pavel Rybakov <pav@finch-melrose.com>
2019-09-12 19:24:11 +0300

    Commit

--- src/test/java/Lib/ui/mobile_web/MWSearchPageObject.java
+++ src/test/java/Lib/ui/mobile_web/MWSearchPageObject.java
@@ -8,7 +8,8 @@ public class MWSearchPageObject extends SearchPageObject {
         SEARCH_INIT_ELEMENT = "css:button#searchIcon";
         SEARCH_INPUT = "css:form>input[type='search']";
         SEARCH_CANCEL_BUTTON = "css:button.cancel";
-        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://div[contains(@class,'wikipedia-description')][contains(text(),'{SUBSTRING}')]";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://div[contains(@class,'wikidata-description')][contains(text(),'{SUBSTRING}')]";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "css:li.title=";
         SEARCH_RESULT_ELEMENT = "css:ul.page-list>li.page-summary";
         SEARCH_EMPTY_RESULTS_ELEMENT = "css:p.without-results";
     }
